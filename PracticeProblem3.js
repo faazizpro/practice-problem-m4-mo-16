cf9fd19c47e07a432170763e40f79e3edfcb9075
faazizpro <fahadafs11@gmail.com>
2022-12-24 16:22:51 +0600

    Problem 3 has been solved

---
+++ PracticeProblem3.js
@@ -0,0 +1,10 @@
+/*
+Practice Problem 3:>
+John's teacher gave him two variables. Each variable contains a string. John's teacher asked him to combine these two strings and print them in one line. Help John write the program.
+*/
+
+let firstLine = "I love code and"
+let secondLine = "I want to be a web developer."
+
+let fullLine = firstLine + ' ' + secondLine;
+console.log(fullLine);

672b831bf350e6e243945cbaced8e9231d8ef4ae
faazizpro <fahadafs11@gmail.com>
2022-12-24 16:12:15 +0600

    Problem 1 has been solved

---
+++ PracticeProblem1.js
@@ -0,0 +1,10 @@
+// Harry's mom gave him money and asked him to buy some oranges and apples. Write a program to help Harry calculate how much money the shopkeeper will return.
+
+let givenTaka = 100;
+let orange1Kg = 40;
+let apple1Kg = 50;
+
+let totalCost = orange1Kg + apple1Kg;
+let remainingTaka = givenTaka - totalCost;
+
+console.log(remainingTaka);
